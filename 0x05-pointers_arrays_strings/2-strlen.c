c00cd1c13b9f76a8af067fc04a5f1a454e987cb0
basele <s.basele@gmail.com>
2023-03-27 13:52:27 -0700

    Task 2 Done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,21 @@
-basele@basele:~/zero_day/alx-low_level_programming/0x05-pointers_arrays_strings$ cat 2-strlen.c
+#include "main.h"
+
 /**
  * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * @s: string parameter input
- *
- * Return: length of string
-*/
-
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
-	int counter;
+	int i;
 
-	for (counter = 0; *s != '\0'; ++s)
-		++counter;
+	i = 0;
 
-	return (counter);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
