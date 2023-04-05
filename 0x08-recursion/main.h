a6c143febc2018978f69301c955369da6c7faba8
basele <s.basele@gmail.com>
2023-04-05 13:53:19 -0700

    0.1 and 2

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *str);
+int _strlen_recursion(char *str);
+int factorial(int s);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int s);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int is_palindrome(char *a);
+int wildcmp(char *t1, char *t2);
+
+#endif
