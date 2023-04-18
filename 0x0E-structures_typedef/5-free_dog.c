edc8b7cd8d7f0494c0f506de1c33191f4e487a3f
basele <s.basele@gmail.com>
2023-04-18 04:28:58 -0700

    Complete Tasks -Dog Struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free struct pointer
+ *
+ * @d: pointer to free
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
